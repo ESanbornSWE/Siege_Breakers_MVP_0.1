a46d01c34a73ea059740fdb87ab0c316114e85df
Ethan Sanborn <ejsanborn@gmail.com>
2024-03-09 19:11:52 -0500

    Changed sprite sheet, adjusted movement controls, added sprites for skeleton mob

--- Player.gd
+++ Player.gd
@@ -3,8 +3,7 @@ extends CharacterBody2D
 @export var move_speed : float = 200
 @export var starting_driection : Vector2 = Vector2(0,1)
 
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -14,22 +13,36 @@ func _physics_process(_delta):
 
 	velocity = input_direction * move_speed
 	
-	update_animation_parameters(input_direction)
-	
 	move_and_slide()
 	
-	pick_new_state()
-
-func _onready():
-	update_animation_parameters(starting_driection)
-
-func update_animation_parameters(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
-
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
+	if velocity == Vector2.ZERO:
+		sprite.play("Idle_Down")
 	else:
-		state_machine.travel("Idle")
+		if input_direction == Vector2(-1,0):
+			sprite.play("Walk_Left")
+		elif input_direction == Vector2(1,0):
+			sprite.play("Walk_Right")
+		elif input_direction == Vector2(0,-1):
+			sprite.play("Walk_Up")
+		elif input_direction == Vector2(0,1):
+			sprite.play("Walk_Down")
+		else:
+			pass
+	
+
+
+
+
+func _on_hurtbox_area_entered(area):
+	pass
+
+
+func _on_hurtbox_body_entered(body):
+	sprite.hide()
+	#TODO add timer to disable this for a short time, blink character back into existence
+	pass # Replace with function body.
+
+
+func _on_hurtbox_body_exited(body):
+	sprite.show()
+	pass # Replace with function body.
---
+++ mob.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 200
+@export var starting_driection : Vector2 = Vector2(0,1)
+
+@onready var sprite = $AnimatedSprite2D
+
+#func _physics_process(_delta):
+	#var input_direction = Vector2(
+		#Input.get_action_strength("right") - Input.get_action_strength("left"),
+		#Input.get_action_strength("down") - Input.get_action_strength("up")
+	#)
+#
+	#velocity = input_direction * move_speed
+	#
+	#move_and_slide()
+	#
+	#if velocity == Vector2.ZERO:
+		#sprite.play("Idle_Down")
+	#else:
+		#if input_direction == Vector2(-1,0):
+			#sprite.play("Walk_Left")
+		#elif input_direction == Vector2(1,0):
+			#sprite.play("Walk_Right")
+		#elif input_direction == Vector2(0,-1):
+			#sprite.play("Walk_Up")
+		#elif input_direction == Vector2(0,1):
+			#sprite.play("Walk_Down")
+		#else:
+			#pass
+	
+
+
+
+
+func _on_hurtbox_area_entered(area):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	sprite.hide()
+	pass # Replace with function body.
